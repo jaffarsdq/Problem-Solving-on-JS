e8a9b0d37e2aa66213590541c4fb026c49e3fbec
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-01-23 12:16:03 +0530

    clean code

--- BasicProblemSolving/LeapYearOrNot.js
+++ BasicProblemSolving/LeapYearOrNot.js
@@ -1,7 +1,7 @@
 function leap(year){
-    if(year%4==0){
-        if(year%100==0){
-            if(year%400==0){
+    if(year % 4 == 0){
+        if(year % 100 == 0){
+            if(year % 400 == 0){
                 return `${year} is leap year`;
             }else{
                 return `${year} is Not a leap year`;
@@ -12,8 +12,7 @@ function leap(year){
     }else{
         return `${year} is Not a leap year`;
     }
-}
+};
 
 let year;
-
 console.log(leap(2100));
