57a643a9a214b5602116a2596f8768cc7318e98a
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-01-25 12:05:37 +0530

    solved

--- PatternSolving/pattern2.js
+++ PatternSolving/pattern2.js
@@ -2,7 +2,7 @@ function pattern(n) {
     for(let i=1 ; i<=n ; i++){
         let str = "";
         for(let j=1 ; j<=i ; j++){
-            str += "*"
+            str += "*";
         }
         console.log(str);
     }
