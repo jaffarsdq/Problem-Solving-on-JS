a7e2ec5ab14d0ca18c53f7134265aee25e91a936
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-01-30 13:57:45 +0530

    FreqMap

---
+++ Problem-Solving-on-JS/BasicPSOnloops/frequencyOfCharacters.js
@@ -0,0 +1,15 @@
+//Problem-Solving-on-JS/BasicProblemSolving/frequencyOFcharacters.js
+let str = "jaffar";
+let freqMap = {};
+function freq(str) {
+    for(let char of str){
+        if(freqMap[char]){
+            freqMap[char] += 1;
+        }else{
+            freqMap[char] = 1;
+        }
+    }
+    return freqMap;
+}
+
+console.log(freq(str));
