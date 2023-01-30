47861d8d444896ad22f5df6391049e86b094e58a
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-01-30 12:49:05 +0530

    codes

--- BasicProblemSolving/TypeOfTriangle.js
+++
@@ -1,8 +0,0 @@
-function Type(a,b,c){
-    // using Ternary Operator
-    return (a==b && b==c && c==a)?("equilateral"):(a!=b && b!=c && c!=a)?("scalene"):("isosceles");
-}
-
-let a = 7,b = 4,c = 5;
-Type(a,b,c);
-console.log(Type(a,b,c));
--- BasicPsOnloops/Print20to1.js
+++
@@ -1,8 +0,0 @@
-function descending(n){
-    for(let i=n ; i>=0 ; i--){
-        console.log(i);
-    }
-}
-
-let n;
-descending(20);
