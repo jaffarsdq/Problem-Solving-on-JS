705cbddd3ccdf15d5ef909b34a020f37d9e5c6b4
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-02-01 17:26:48 +0530

    added

--- Problem-Solving-on-JS/LeetCodePS.js/5TwoSum(nested loop)method.js
+++ Problem-Solving-on-JS/LeetCodePS.js/5TwoSum(nested loop)method.js
@@ -5,7 +5,7 @@ let nums = [2,7,11,15], target = 9;
 console.log(twoSum(nums,target));
 
 function twoSum(nums,target) {
-    for(let i=0; i<nums.length; i++){
+    for(let i=0; i<nums.length-1; i++){
         for(let j= i+1; j<nums.length; j++){
             if(nums[i] + nums[j] == target) return [i,j];
         }
